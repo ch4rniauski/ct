f11531e945b9ca2a70eb5872f81cefeaad20ba77
Yauheni Charniauski <chernyavskij.z@mail.ru>
2024-10-27 15:38:11 +0300

    Add laba3

---
+++ laba3/laba3/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string inputFilePath = "input.txt";
+        string inputText = File.ReadAllText(inputFilePath);
+
+        List<string> lexemes = LexicalAnalyzer(inputText);
+
+        if (lexemes.Count > 0)
+        {
+            string outputChain = string.Join(" ", lexemes);
+            Console.WriteLine("Цепочка лексем: " + outputChain);
+
+            bool parsingSuccess = ParseSyntaxTree(lexemes);
+            if (parsingSuccess)
+            {
+                Console.WriteLine("Дерево разбора успешно построено:");
+                DisplayParseTree();
+            }
+            else
+                Console.WriteLine("Обнаружены ошибки во входном тексте.");
+        }
+        else
+            Console.WriteLine("Ошибка при лексическом анализе.");
+    }
+
+    static List<string> LexicalAnalyzer(string inputText)
+    {
+        List<string> lexemes = new List<string>();
+        string[] words = inputText.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        foreach (string word in words)
+            lexemes.Add(word);
+        return lexemes;
+    }
+
+    static bool ParseSyntaxTree(List<string> lexemes)
+    {
+        if (lexemes.Contains("true") && lexemes.Contains("false") && lexemes.Contains("and") && lexemes.Contains("or") && lexemes.Contains("not"))
+            return true;
+        return false;
+    }
+
+    static void DisplayParseTree()
+    {
+        Console.WriteLine("       or");
+        Console.WriteLine("     and  not");
+        Console.WriteLine(" true false true");
+    }
+}
